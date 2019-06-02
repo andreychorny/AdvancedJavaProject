d46cad6749bfe65fbef0fc12718768f10a2be03f
Andrii Chornyi <andrey-evil@ukr.net>
2019-06-02 21:24:20 +0200

    Added Maven support, added dependency on log4j

--- src/main/java/BankOne/com/BankData/Bank.java
+++ src/main/java/BankOne/com/BankData/Bank.java
@@ -1,6 +1,8 @@
 package BankOne.com.BankData;
 
 import BankOne.com.accounts.Account;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.util.ArrayList;
 import java.util.Date;
@@ -8,6 +10,8 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Bank {
+
+    static Logger logger = LogManager.getLogger(Bank.class);
 
     static List<Customer> customers = new ArrayList<>();
 
@@ -30,38 +34,38 @@ public class Bank {
         yearOfBirth = yearOfBirth - 1900;
         System.out.println("Enter month(number) of birth");
         int monthOfBirth = in.nextInt();
-        monthOfBirth = monthOfBirth - 1 ;
+        monthOfBirth = monthOfBirth - 1;
         System.out.println("Enter day of birth");
         int dayOfBirth = in.nextInt();
         customers.add(new Customer(firstName, lastName, new Date(yearOfBirth, monthOfBirth, dayOfBirth), new Date()));
     }
 
-    static Account findAccount(String number){
-        for(Customer customer : customers){
-            for(Account account : customer.getAccounts()){
-                if(account.getNumber().equals(number)) return account;
+    static Account findAccount(String number) {
+        for (Customer customer : customers) {
+            for (Account account : customer.getAccounts()) {
+                if (account.getNumber().equals(number)) return account;
             }
         }
         return null;
     }
 
-    static void outputAllAccounts(){
-        for(Customer customer : customers){
-            System.out.println("Customer: " + customer.getFirstName() +" " + customer.getLastName() + ":");
-            for(Account account : customer.getAccounts()){
+    static void outputAllAccounts() {
+        for (Customer customer : customers) {
+            System.out.println("Customer: " + customer.getFirstName() + " " + customer.getLastName() + ":");
+            for (Account account : customer.getAccounts()) {
                 Class accClass = account.getClass();
                 String nameOfClass = accClass.toString();
-                nameOfClass = nameOfClass.substring(nameOfClass.lastIndexOf(".")+1);
-                System.out.println( nameOfClass+": " + account.getNumber());
+                nameOfClass = nameOfClass.substring(nameOfClass.lastIndexOf(".") + 1);
+                System.out.println(nameOfClass + ": " + account.getNumber());
                 System.out.println("amount of money: " + account.getAmountOfMoney());
             }
         }
     }
 
-    static boolean checkIfNumberUnique(String number){
-        for(Customer customer : customers){
-            for(Account account : customer.getAccounts()){
-                if(account.getNumber().equals(number)) return false;
+    static boolean checkIfNumberUnique(String number) {
+        for (Customer customer : customers) {
+            for (Account account : customer.getAccounts()) {
+                if (account.getNumber().equals(number)) return false;
             }
         }
         return true;
