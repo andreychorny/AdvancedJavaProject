a63011eedef479c729b870ffdf4ecc8659e6efb9
Andrii Chornyi <andrey-evil@ukr.net>
2019-06-28 03:07:40 +0200

    fix codesmell BankJUnit

--- src/test/java/bankone/BankJUnit.java
+++ src/test/java/bankone/BankJUnit.java
@@ -11,6 +11,10 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 public class BankJUnit {
 
     private Bank bank;
+    private final String RIGHT_PASS_FORM = "bravos";
+    private final String RIGHT_LOG_FORM = "johnny";
+    private final String RIGHT_NAME_FORM = "Name";
+    private final String RIGHT_LASTNAME_FORM = "lastName";
 
     @BeforeEach
     void setUpbank() {
@@ -43,21 +47,21 @@ public class BankJUnit {
 
     @Test
     void testValidationOfNameAndLastName() {
-        assertThrows(IllegalArgumentException.class, () -> bank.createNewEmployee("johnny", "bravos",
-                "Name1", "lastName"), "Number in name - bad format");
-        assertThrows(IllegalArgumentException.class, () -> bank.createNewEmployee("johnny", "bravos",
-                "Name", "lastName!"), "Special symbol in lastName - bad format");
-        assertThrows(IllegalArgumentException.class, () -> bank.createNewEmployee("johnny", "bravos",
-                "Name", "Z"), "LastName is too short");
-        assertThrows(IllegalArgumentException.class, () -> bank.createNewEmployee("johnny", "bravos",
-                "K", "Zack"), "Name is too short");
+        assertThrows(IllegalArgumentException.class, () -> bank.createNewEmployee(RIGHT_LOG_FORM, RIGHT_PASS_FORM,
+                "Name1", RIGHT_LASTNAME_FORM), "Number in name - bad format");
+        assertThrows(IllegalArgumentException.class, () -> bank.createNewEmployee(RIGHT_LOG_FORM, RIGHT_PASS_FORM,
+                RIGHT_NAME_FORM, "lastName!"), "Special symbol in lastName - bad format");
+        assertThrows(IllegalArgumentException.class, () -> bank.createNewEmployee(RIGHT_LOG_FORM, RIGHT_PASS_FORM,
+                RIGHT_NAME_FORM, "Z"), "LastName is too short");
+        assertThrows(IllegalArgumentException.class, () -> bank.createNewEmployee(RIGHT_LOG_FORM, RIGHT_PASS_FORM,
+                "K", RIGHT_LASTNAME_FORM), "Name is too short");
     }
 
     @Test
     void testValidationOfLoginAndPassword() {
-        assertThrows(IllegalArgumentException.class, () -> bank.createNewEmployee("johnny?", "bravos", "Name",
-                "lastName"), "Special symbol in login - bad format");
-        assertThrows(IllegalArgumentException.class, () -> bank.createNewEmployee("johnny", "bravo", "Name",
-                "lastName"), "password to short - bad format");
+        assertThrows(IllegalArgumentException.class, () -> bank.createNewEmployee("johnny?", RIGHT_PASS_FORM, RIGHT_NAME_FORM,
+                RIGHT_LASTNAME_FORM), "Special symbol in login - bad format");
+        assertThrows(IllegalArgumentException.class, () -> bank.createNewEmployee(RIGHT_LOG_FORM, "bravo", RIGHT_NAME_FORM,
+                RIGHT_LASTNAME_FORM), "password to short - bad format");
     }
 }
